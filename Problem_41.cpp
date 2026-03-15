0622509f6e3d6447c124d8fc4ac9aff37d3544f8
AyyoubBelkher <ayyoubbelkher1@gmail.com>
2026-03-15 21:06:52 +0000

    Problem 41: Convert hours to days and weeks using modular functions

---
+++ Problem_41.cpp
@@ -0,0 +1,59 @@
+/*
+Problem 41:
+Write a program to read a NumberOfHours and calculates the number of weeks, and days included in that
+number
+Input
+365
+Outputs
+
+2.17 Weeks
+15.20 Days*/
+
+#include <iostream>
+
+using namespace std;
+
+float ReadPositiveNumber(string Message)
+{
+    float Number = 0;
+
+    do
+    {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+float HoursToDays(float NumberOfHours)
+{
+    return (float)NumberOfHours / 24;
+}
+
+float HoursToWeeks(float NumberOfHours)
+{
+    return (float)NumberOfHours / (24 * 7);
+}
+
+float DaysToWeeks(float NumberOfDays)
+{
+    return (float)NumberOfDays / 7;
+}
+
+int main()
+{
+    float NumberOfHours = ReadPositiveNumber("Please Enter Number Of Hours?");
+
+    float NumberOfDays = HoursToDays(NumberOfHours);
+
+    float NumberOfWeeks = DaysToWeeks(NumberOfDays);
+
+    cout << endl;
+
+    cout << "Total Hours = " << NumberOfHours << endl;
+    cout << "Total Days = " << NumberOfDays << endl;
+    cout << "Total Weeks = " << HoursToWeeks(NumberOfHours) << endl;
+
+    return 0;
+}
